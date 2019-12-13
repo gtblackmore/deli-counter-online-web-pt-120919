4734b58bb3c9c15d91c8e4f4c8f1481ca7527b77
George Blackmore <gtblackmore@gmail.com>
2019-12-13 05:27:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+  customers = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+    count = 1
+    katz_deli.each_index do |index, customer|
+      
+    customers.push(Katz_deli)
+    count = count + 1
+    puts "The line is currently: "
+    end
+  end
+end
+  
